e55aac5ee4019573a3912f0edfe49aa77347116d
Kale-89 <kalehiwotm@gmail.com>
2026-06-18 18:01:30 +0300

    bug fix in the campaignService.js

--- server/services/campaignService.js
+++ server/services/campaignService.js
@@ -1,4 +1,4 @@
-import { supabase } from "../config/supabase.js";
+import supabase from "../config/supabase.js";
 
 async function getAllCampaigns() {
   const { data, error } = await supabase.from("campaigns").select("*");
